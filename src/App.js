175fc68af4eb95cf6d5a0042702afac31d64c203
drod702 <drod702@yahoo.com>
2021-11-04 14:48:47 -0700

    set up elements needed

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 
 function App() {
+  const [quote, setQuote] = useState('The most difficult thing is the decision to act.')
+  // let quote = 'The most difficult thing is the decision to act, the rest is merely tenacity.'
+  const [author, setAuthor] = useState('Amelia Earhart')
+  // let author = 'Amelia Earhart'
+
+  const changeQuoteAndAuthor = () => {
+    setQuote('Dreaming, after all, is a form of planning.');
+    setAuthor('Gloria Steinem')
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <p>
-          This my Hello World!
+          "{quote}"
         </p>
+        <p>
+          - {author}
+        </p>
+
+        <button onClick={() => changeQuoteAndAuthor()}>Change Quote</button>
       </header>
     </div>
   );
