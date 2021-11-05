5661649d5b6a00754c1138fa49863339a79335d4
drod702 <drod702@yahoo.com>
2021-11-04 18:53:18 -0700

    assigned random selection

--- src/App.js
+++ src/App.js
@@ -3,26 +3,55 @@ import './App.scss';
 
 function App() {
   const [quote, setQuote] = useState('The most difficult thing is the decision to act.')
-  // let quote = 'The most difficult thing is the decision to act, the rest is merely tenacity.'
   const [author, setAuthor] = useState('Amelia Earhart')
-  // let author = 'Amelia Earhart'
+  const [randomNumber, setRandomNumber] = useState(0)
 
-  const changeQuoteAndAuthor = () => {
+  const changeQuoteAndAuthor0 = () => {
+    setQuote('The most difficult thing is the decision to act.');
+    setAuthor('Amelia Earhart')
+  }
+
+  const changeQuoteAndAuthor1 = () => {
     setQuote('Dreaming, after all, is a form of planning.');
     setAuthor('Gloria Steinem')
+  }
+
+  const changeQuoteAndAuthor2 = () => {
+    setQuote('People often say that motivation doesn’t last. Well, neither does bathing. That’s why we recommend it daily.');
+    setAuthor('Zig Ziglar')
+  }
+
+  const genRandomNumber = () => {
+    let randomNum = Math.floor(Math.random() * 3)
+    setRandomNumber(randomNum)
+
+    if (randomNum === 0) {
+      changeQuoteAndAuthor0()
+    }
+
+    if (randomNum === 1) {
+      changeQuoteAndAuthor1()
+    }
+
+    if (randomNum === 2) {
+      changeQuoteAndAuthor2()
+    }
   }
 
   return (
     <div className="App">
       <header className="App-header">
+        <h1>Random Number: { randomNumber }</h1>
+        <button onClick = {() => genRandomNumber()}> Random Number </button>
         <p>
           "{quote}"
         </p>
         <p>
           - {author}
         </p>
-
-        <button onClick={() => changeQuoteAndAuthor()}>Change Quote</button>
+        <button onClick = {() => changeQuoteAndAuthor0()}>Change Quote0</button>
+        <button onClick = {() => changeQuoteAndAuthor1()}>Change Quote1</button>
+        <button onClick = {() => changeQuoteAndAuthor2()}>Change Quote2</button>
       </header>
     </div>
   );
